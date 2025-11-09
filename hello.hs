74e2823e231d95882f169a06351de8e5ad3b9a6f
Rottschaefer <rottschaefer54@gmail.com>
2025-11-09 17:38:43 -0300

    Estrutura inicial feita, pegando input do arquivo e transformando numa lista de Int

--- hello.hs
+++ hello.hs
@@ -1,4 +1,13 @@
 main = do
     content <- readFile "exe.txt"
-    putStr content
+    mapM_ putStrLn $ map resolverPasseio $ map processarLinha $ lines content
+
+--Recebe a linha lida do arquivo e retorna uma tupla com n, m e a posição (x,y)
+processarLinha :: String -> [Int]
+processarLinha linha = 
+    map read $ words linha :: [Int]
+
     
+resolverPasseio :: [Int] -> String
+resolverPasseio [n, m, x, y] =
+    "Tabuleiro " ++ show n ++ "x" ++ show m ++ " de (" ++ show x ++ ", " ++ show y ++ ")\n"
